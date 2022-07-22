5803594cd0f947b8c2f1a295d3ac570cebe76f79
deynega <deynega.a@gmail.com>
2022-07-22 22:19:26 +0800

    logo target

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // initialize MindAR 
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: './src/assets/targets/card.mind',
+      imageTargetSrc: './src/assets/targets/test4.mind',
     });
     const {renderer, scene, camera} = mindarThree;
 
