64687e2142d6afbe9b5fa9f0b22c689bc4dbdb97
deynega <deynega.a@gmail.com>
2022-07-25 18:02:41 +0800

    Add animation

--- main.js
+++ main.js
@@ -1,30 +1,65 @@
 const THREE = window.MINDAR.IMAGE.THREE;
+import {loadGLTF} from "/src/applications/libs/loader.js";
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('started');
+
+  const isiPhone = navigator.userAgent.indexOf(' iPhone ') >= 0
+
+  console.log('navigator.userAgent - ' + JSON.stringify(navigator.userAgent));
+
+
   const start = async() => {
     // initialize MindAR 
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
       imageTargetSrc: './src/assets/targets/card.mind',
     });
+
+    mindarThree.switchCamera();
     const {renderer, scene, camera} = mindarThree;
 
-    // create AR object
-    const geometry = new THREE.PlaneGeometry(1, 1);
-    const material = new THREE.MeshBasicMaterial({color: 0x00ffff, transparent: true, opacity: 0.5});
-    const plane = new THREE.Mesh(geometry, material);
+    // add light
+    const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
+    scene.add(light);
 
-    // create anchor
+    // add gltf model
+    const raccoon = await loadGLTF('/src/assets/models/musicband-raccoon/scene.gltf');
+    raccoon.scene.scale.set(0.1, 0.1, 0.1);
+    raccoon.scene.position.set(0, -0.4, 0);
+
+
+    // add model to anchor
     const anchor = mindarThree.addAnchor(0);
-    anchor.group.add(plane);
+    anchor.group.add(raccoon.scene);
 
-    // start AR
-    console.log('start AR---');
+    // get animation
+    const mixer = new THREE.AnimationMixer(raccoon.scene);
+    const action = mixer.clipAction(raccoon.animations[0]);
+    action.play();
+
+    const clock = new THREE.Clock();
+
+
+    anchor.onTargetFound = () => {
+      console.log("on target found");
+    }
+    anchor.onTargetLost = () => {
+      console.log("on target lost");
+    }
+
+    document.querySelector("#switch").addEventListener("click", () => {
+      console.log('switch camera');
+      mindarThree.switchCamera();
+    });
+    
+    
+    
     await mindarThree.start();
-    console.log('mindarThree started');
     renderer.setAnimationLoop(() => {
-      console.log('render');
+      const delta = clock.getDelta();
+      // raccoon.scene.rotation.set(0, raccoon.scene.rotation.y+delta, 0);
+      mixer.update(delta);
       renderer.render(scene, camera);
     });
   }
