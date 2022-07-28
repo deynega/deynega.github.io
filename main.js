e7e5ff87596a18e62898ead4cb8b3ba3f2100fc7
deynega <deynega.a@gmail.com>
2022-07-28 15:40:37 +0800

    scale 5

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // initialize MindAR 
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.querySelector("#container"),
-      imageTargetSrc: './src/assets/targets/icvr.mind',
+      imageTargetSrc: './src/assets/targets/icvrScale5.mind',
       uiScanning: '#scanning',
       uiLoading: 'yes'
     });
