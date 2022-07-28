123db5a45327893640701a7465c9024f6a592598
deynega <deynega.a@gmail.com>
2022-07-28 15:28:55 +0800

    icvr.mind

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // initialize MindAR 
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.querySelector("#container"),
-      imageTargetSrc: './src/assets/targets/card.mind',
+      imageTargetSrc: './src/assets/targets/icvr.mind',
       uiScanning: '#scanning',
       uiLoading: 'yes'
     });
